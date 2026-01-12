c1776067599909069afeeffb19bf374eb11d65ad
Andrei Paul Hospital <afhospital@student.apc.edu.ph>
2026-01-12 06:51:07 +0000

    dreiii

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
+<script></script>
 
-<template>
-  <h1>You did it Andrei!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<template>{{message}}</h1>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Dreiiii'
+    };
+  }
+};
+</script>
+
+
+<style></style>
+
