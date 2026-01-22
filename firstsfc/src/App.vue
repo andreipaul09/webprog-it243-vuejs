b29f47dd8fb85c843a291bc75a50d64ee6004de9
Andrei Paul Hospital <afhospital@student.apc.edu.ph>
2026-01-22 14:47:16 +0800

    adding supabase client

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,15 +1,21 @@
-<template>
-  <h1>{{ message }}</h1>
-</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-<script>
-export default {
-  data() {
-    return {
-      message: 'dreiiii09'
-    };
-  }
-};
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
 </script>
 
-<style></style>
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
