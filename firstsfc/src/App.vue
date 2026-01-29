38577c0281d899860b7d36ffef0af227d5d607ab
Andrei Paul Hospital <afhospital@student.apc.edu.ph>
2026-01-29 14:12:49 +0800

    adding commentform and comment

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,21 +1,12 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-
-onMounted(() => {
-   getInstruments()
-})
-</script>
-
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <h1>My Personal Page</h1>
+  <country/>
+  <personal profile/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
+
+<script></script>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('food-item', FoodItem)
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+app.mount('#app')
