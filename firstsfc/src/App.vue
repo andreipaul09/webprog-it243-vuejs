cd3307979a080200a1b4563bf36d0dc1b04d7519
Andrei Paul Hospital <afhospital@student.apc.edu.ph>
2026-02-21 00:59:01 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,21 +1,10 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
- 
-const instruments = ref([])
- 
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
- 
-onMounted(() => {
-   getInstruments()
-})
-</script>
- 
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <h1>FOOD</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
+
+<script></script>
+
+<style></style>
